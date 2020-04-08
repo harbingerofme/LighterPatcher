74b648670436d0143b4040199301df480e35450c
Harbingerofme <harbingerofme@gmail.com>
2020-04-08 14:34:02 +0200

    test program

---
+++ cecilTests/Program.cs
@@ -0,0 +1,20 @@
+﻿using Mono.Cecil;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace cecilTests
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var a = AssemblyDefinition.ReadAssembly(args[0]);
+            List<TypeDefinition> list = a.MainModule.Types.ToList();
+            foreach (TypeDefinition type in list.OrderBy(x => x.FullName))
+            {
+                Console.WriteLine(type.FullName);
+            }
+        }
+    }
+}
